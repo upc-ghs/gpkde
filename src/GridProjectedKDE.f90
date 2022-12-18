fe527a61b607eedc74698308c66e18ce1650bc8d
Rodrigo Pérez <rodrigo.alfonso.perez@upc.edu>
2022-12-18 09:14:36 +0100

    default initial smoothing factor eq 1

--- src/GridProjectedKDE.f90
+++ src/GridProjectedKDE.f90
@@ -34,7 +34,7 @@ module GridProjectedKDEModule
     logical, parameter ::  defaultAnisotropicSigmaSupport = .false.
 
     ! Optimization
-    doubleprecision :: defaultInitialSmoothingFactor = 3d0
+    doubleprecision :: defaultInitialSmoothingFactor = 1d0
     doubleprecision :: defaultDensityScale           = 1d0
     !doubleprecision :: defaultMinLimitRoughness      = 1d-4
     !doubleprecision :: defaultMaxLimitRoughness      = 1d4
